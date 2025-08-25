ae25b66fa97b11e2079f96edd66578f0f6e16a12
Derrick Kwaku Togodui <125933236+derricktogodui@users.noreply.github.com>
2025-08-25 01:42:46 -1200

    Update mathfriend.py

--- mathfriend.py
+++ mathfriend.py
@@ -191,7 +191,7 @@ def create_and_verify_tables():
         print("Database tables created or verified successfully, including corrected Duel tables.")
     except Exception as e:
         st.error(f"Database setup error: {e}")
-#create_and_verify_tables()
+create_and_verify_tables()
 
 
 # --- Core Backend Functions (PostgreSQL) ---
@@ -4155,6 +4155,7 @@ else:
         show_main_app()
     else:
         show_login_or_signup_page()
+
 
 
 
