8ab39fb43c117b11f10bf694e37bdfb664617918
Derrick Kwaku Togodui <125933236+derricktogodui@users.noreply.github.com>
2025-09-04 14:26:43 -1200

    Update mathfriend.py

--- mathfriend.py
+++ mathfriend.py
@@ -5235,7 +5235,7 @@ def display_quiz_page(topic_options):
         with col2:
             with st.container(border=True):
                 st.markdown("#### 🚀 WASSCE Prep")
-                st.caption("A 20-question, mixed-topic challenge to test your exam readiness!")
+                st.caption("A 40-question, mixed-topic challenge to test your exam readiness!")
                 st.markdown(f'<img src="https://github.com/derricktogodui/mathfriend-app/releases/download/v1.0-assets/WASSCE.Study.Session.in.Action.png" class="quiz-prep-image">', unsafe_allow_html=True)
                 if st.button("Start Exam Prep", key="start_wassce", type="primary", use_container_width=True):
                     st.session_state.is_wassce_mode = True
@@ -7063,6 +7063,7 @@ else:
         show_main_app()
     else:
         show_login_or_signup_page()
+
 
 
 
